b730782a8927c99477c6429603a6ce14cf521a55
Pedrowsky02 <ptorress@unal.edu.co>
2026-07-08 20:58:15 -0500

    Update AppBeta.py

--- AppBeta.py
+++ AppBeta.py
@@ -54,7 +54,7 @@ if "pagina" not in st.session_state:
     
 def inicio():    
     st.write("# Bienvenido al Banco de Intercambiadores Digital")
-    st.write("##### Un gemelo digital del banco de intercambiadores de calor del laboratorio de ingeniería quimica de la UNAL, para aprender, enseñar y entrenar la transferencia de calor.")
+    st.text("Un gemelo digital del banco de intercambiadores de calor del laboratorio de ingeniería quimica de la UNAL, para aprender, enseñar y entrenar la transferencia de calor.")
     st.write("## Instructivo del Gemelo Digital")
     st.video("https://youtu.be/TU8fJsVeWc0")
 
