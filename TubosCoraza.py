46f6fc2f526b31f91f74915df2e34e1d0fd3abc2
Pedrowsky02 <ptorress@unal.edu.co>
2026-07-09 11:44:00 -0500

    Update TubosCoraza.py

--- TubosCoraza.py
+++ TubosCoraza.py
@@ -140,94 +140,93 @@ def TubosyCoraza():
         if st.button("Cambiar configuración",width="stretch"):
             st.session_state.Configuracion_TC = None
             st.rerun()
-
-#         if st.session_state.Configuracion_TC == "Pitch Triangular - 1 Paso":
-#             ho_vapor = 1200
-#             hi_agua = 900
-#             ho_agua = 700
-#             col1,col2 = st.columns(2)
-#             with col1:
-#                 QAF = st.number_input("Especifique el caudal del fluido frio (mL/s)", min_value=1, max_value=1000,step=1)
-#             with col2:
-#                 TAF = st.slider("Especifique la temperatura de ingreso del fluido frio (°C)", min_value=15, max_value=30, step=1)
-#             MetCal = st.selectbox("Seleccione el fluido de calentamiento",{"Agua Caliente","Vapor"})
-#             if MetCal == "Agua Caliente":
-#                 ConfigFlow = st.selectbox("Seleccione la configuración de flujo",["Paralelo","Contraflujo"])
-#                 col1, col2 = st.columns(2)
-#                 with col1:
-#                     QAC = st.number_input("Especifique el caudal del fluido caliente (mL/s)", min_value=1, max_value=1000,step=1)
-#                 with col2:
-#                     TAC = st.slider("Especifique la temperatura de ingreso del fluido Caliente (°C)", min_value=TAF+1, max_value=80, step=1)
-#             else:
-#                 T_sat = st.slider("Especifique la temperatura de ingreso del vapor (°C)",min_value=93,max_value=150,step=1)
-#             if st.button("Correr Simulación") == True:
-#                 pass
+        if st.session_state.Configuracion_TC == "Pitch Triangular - 1 Paso":
+            ho_vapor = 1200
+            hi_agua = 900
+            ho_agua = 700
+            col1,col2 = st.columns(2)
+            with col1:
+                QAF = st.number_input("Especifique el caudal del fluido frio (mL/s)", min_value=1, max_value=1000,step=1)
+            with col2:
+                TAF = st.slider("Especifique la temperatura de ingreso del fluido frio (°C)", min_value=15, max_value=30, step=1)
+            MetCal = st.selectbox("Seleccione el fluido de calentamiento",{"Agua Caliente","Vapor"})
+            if MetCal == "Agua Caliente":
+                ConfigFlow = st.selectbox("Seleccione la configuración de flujo",["Paralelo","Contraflujo"])
+                col1, col2 = st.columns(2)
+                with col1:
+                    QAC = st.number_input("Especifique el caudal del fluido caliente (mL/s)", min_value=1, max_value=1000,step=1)
+                with col2:
+                    TAC = st.slider("Especifique la temperatura de ingreso del fluido Caliente (°C)", min_value=TAF+1, max_value=80, step=1)
+            else:
+                T_sat = st.slider("Especifique la temperatura de ingreso del vapor (°C)",min_value=93,max_value=150,step=1)
+            if st.button("Correr Simulación") == True:
+                pass
             
             
-#         elif st.session_state.Configuracion_TC == "Pitch Triangular - 2 Paso":
-#             ho_vapor = 1400
-#             hi_agua = 1100
-#             ho_agua = 850
-#             col1,col2 = st.columns(2)
-#             with col1:
-#                 QAF = st.number_input("Especifique el caudal del fluido frio (mL/s)", min_value=1, max_value=1000,step=1)
-#             with col2:
-#                 TAF = st.slider("Especifique la temperatura de ingreso del fluido frio (°C)", min_value=15, max_value=30, step=1)
-#             MetCal = st.selectbox("Seleccione el fluido de calentamiento",{"Agua Caliente","Vapor"})
-#             if MetCal == "Agua Caliente":
-#                 ConfigFlow = st.selectbox("Seleccione la configuración de flujo",["Paralelo","Contraflujo"])
-#                 col1, col2 = st.columns(2)
-#                 with col1:
-#                     QAC = st.number_input("Especifique el caudal del fluido caliente (mL/s)", min_value=1, max_value=1000,step=1)
-#                 with col2:
-#                     TAC = st.slider("Especifique la temperatura de ingreso del fluido Caliente (°C)", min_value=TAF+1, max_value=80, step=1)
-#             else:
-#                 T_sat = st.slider("Especifique la temperatura de ingreso del vapor (°C)",min_value=93,max_value=150,step=1)
-#             if st.button("Correr Simulación") == True:
-#                 pass
+        elif st.session_state.Configuracion_TC == "Pitch Triangular - 2 Paso":
+            ho_vapor = 1400
+            hi_agua = 1100
+            ho_agua = 850
+            col1,col2 = st.columns(2)
+            with col1:
+                QAF = st.number_input("Especifique el caudal del fluido frio (mL/s)", min_value=1, max_value=1000,step=1)
+            with col2:
+                TAF = st.slider("Especifique la temperatura de ingreso del fluido frio (°C)", min_value=15, max_value=30, step=1)
+            MetCal = st.selectbox("Seleccione el fluido de calentamiento",{"Agua Caliente","Vapor"})
+            if MetCal == "Agua Caliente":
+                ConfigFlow = st.selectbox("Seleccione la configuración de flujo",["Paralelo","Contraflujo"])
+                col1, col2 = st.columns(2)
+                with col1:
+                    QAC = st.number_input("Especifique el caudal del fluido caliente (mL/s)", min_value=1, max_value=1000,step=1)
+                with col2:
+                    TAC = st.slider("Especifique la temperatura de ingreso del fluido Caliente (°C)", min_value=TAF+1, max_value=80, step=1)
+            else:
+                T_sat = st.slider("Especifique la temperatura de ingreso del vapor (°C)",min_value=93,max_value=150,step=1)
+            if st.button("Correr Simulación") == True:
+                pass
 
 
-#         elif st.session_state.Configuracion_TC == "Pitch Triangular - Sin Bafles":
-#             ho_vapor = 700
-#             hi_agua = 600
-#             ho_agua = 350
-#             col1,col2 = st.columns(2)
-#             with col1:
-#                 QAF = st.number_input("Especifique el caudal del fluido frio (mL/s)", min_value=1, max_value=1000,step=1)
-#             with col2:
-#                 TAF = st.slider("Especifique la temperatura de ingreso del fluido frio (°C)", min_value=15, max_value=30, step=1)
-#             MetCal = st.selectbox("Seleccione el fluido de calentamiento",{"Agua Caliente","Vapor"})
-#             if MetCal == "Agua Caliente":
-#                 ConfigFlow = st.selectbox("Seleccione la configuración de flujo",["Paralelo","Contraflujo"])
-#                 col1, col2 = st.columns(2)
-#                 with col1:
-#                     QAC = st.number_input("Especifique el caudal del fluido caliente (mL/s)", min_value=1, max_value=1000,step=1)
-#                 with col2:
-#                     TAC = st.slider("Especifique la temperatura de ingreso del fluido Caliente (°C)", min_value=TAF+1, max_value=80, step=1)
-#             else:
-#                 T_sat = st.slider("Especifique la temperatura de ingreso del vapor (°C)",min_value=93,max_value=150,step=1)
-#             if st.button("Correr Simulación") == True:
-#                 pass
+        elif st.session_state.Configuracion_TC == "Pitch Triangular - Sin Bafles":
+            ho_vapor = 700
+            hi_agua = 600
+            ho_agua = 350
+            col1,col2 = st.columns(2)
+            with col1:
+                QAF = st.number_input("Especifique el caudal del fluido frio (mL/s)", min_value=1, max_value=1000,step=1)
+            with col2:
+                TAF = st.slider("Especifique la temperatura de ingreso del fluido frio (°C)", min_value=15, max_value=30, step=1)
+            MetCal = st.selectbox("Seleccione el fluido de calentamiento",{"Agua Caliente","Vapor"})
+            if MetCal == "Agua Caliente":
+                ConfigFlow = st.selectbox("Seleccione la configuración de flujo",["Paralelo","Contraflujo"])
+                col1, col2 = st.columns(2)
+                with col1:
+                    QAC = st.number_input("Especifique el caudal del fluido caliente (mL/s)", min_value=1, max_value=1000,step=1)
+                with col2:
+                    TAC = st.slider("Especifique la temperatura de ingreso del fluido Caliente (°C)", min_value=TAF+1, max_value=80, step=1)
+            else:
+                T_sat = st.slider("Especifique la temperatura de ingreso del vapor (°C)",min_value=93,max_value=150,step=1)
+            if st.button("Correr Simulación") == True:
+                pass
 
-#         elif st.session_state.Configuracion_TC == "Pitch Cuadrado - 1 Paso":
-#             ho_vapor = 1000
-#             hi_agua = 850
-#             ho_agua = 600
-#             col1,col2 = st.columns(2)
-#             with col1:
-#                 QAF = st.number_input("Especifique el caudal del fluido frio (mL/s)", min_value=1, max_value=1000,step=1)
-#             with col2:
-#                 TAF = st.slider("Especifique la temperatura de ingreso del fluido frio (°C)", min_value=15, max_value=30, step=1)
-#             MetCal = st.selectbox("Seleccione el fluido de calentamiento",{"Agua Caliente","Vapor"})
-#             if MetCal == "Agua Caliente":
-#                 ConfigFlow = st.selectbox("Seleccione la configuración de flujo",["Paralelo","Contraflujo"])
-#                 col1, col2 = st.columns(2)
-#                 with col1:
-#                     QAC = st.number_input("Especifique el caudal del fluido caliente (mL/s)", min_value=1, max_value=1000,step=1)
-#                 with col2:
-#                     TAC = st.slider("Especifique la temperatura de ingreso del fluido Caliente (°C)", min_value=TAF+1, max_value=80, step=1)
-#             else:
-#                 T_sat = st.slider("Especifique la temperatura de ingreso del vapor (°C)",min_value=93,max_value=150,step=1)
-#             if st.button("Correr Simulación") == True:
-#                 pass
+        elif st.session_state.Configuracion_TC == "Pitch Cuadrado - 1 Paso":
+            ho_vapor = 1000
+            hi_agua = 850
+            ho_agua = 600
+            col1,col2 = st.columns(2)
+            with col1:
+                QAF = st.number_input("Especifique el caudal del fluido frio (mL/s)", min_value=1, max_value=1000,step=1)
+            with col2:
+                TAF = st.slider("Especifique la temperatura de ingreso del fluido frio (°C)", min_value=15, max_value=30, step=1)
+            MetCal = st.selectbox("Seleccione el fluido de calentamiento",{"Agua Caliente","Vapor"})
+            if MetCal == "Agua Caliente":
+                ConfigFlow = st.selectbox("Seleccione la configuración de flujo",["Paralelo","Contraflujo"])
+                col1, col2 = st.columns(2)
+                with col1:
+                    QAC = st.number_input("Especifique el caudal del fluido caliente (mL/s)", min_value=1, max_value=1000,step=1)
+                with col2:
+                    TAC = st.slider("Especifique la temperatura de ingreso del fluido Caliente (°C)", min_value=TAF+1, max_value=80, step=1)
+            else:
+                T_sat = st.slider("Especifique la temperatura de ingreso del vapor (°C)",min_value=93,max_value=150,step=1)
+            if st.button("Correr Simulación") == True:
+                pass
         
